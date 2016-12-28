32fe19c2fb05d169fe3244087bbf8a0c800efaaa
Wayne Starr <wes7817@g.rit.edu>
2016-12-28 16:17:25 -0700

    renames tokenizer to wordtokenizer to avoid conflicts

--- tokenizer.js
+++ tokenizer.js
@@ -1,10 +1,10 @@
 const natural = require('natural');
 
-var tokenizer = new natural.WordTokenizer();
+var wordtokenizer = new natural.WordTokenizer();
 
 module.exports = {
   check: function ( triggerPhrase, tokens ) {
-    var newTokens = tokenizer.tokenize( triggerPhrase );
+    var newTokens = wordtokenizer.tokenize( triggerPhrase );
     var numTokens = newTokens.length;
 
     for (i = 0; i < numTokens; i++) {
