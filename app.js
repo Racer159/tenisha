8f13bcf5418d7486d9be02a53d9928aa0db3bbc6
Wayne Starr <wes7817@g.rit.edu>
2016-12-28 17:52:09 -0700

    added a sying on startup

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var bot = fancybot(config);
 
 bot.on('botRegistered', function() {
 	console.log("online");
+	bot.message("Hey y'all. Hit me up at 'tenisha' if you need me.");
 });
 
 bot.on('botMessage', function(bot, message) {
