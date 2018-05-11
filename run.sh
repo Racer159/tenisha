dd51e962618cfb63aadabd4c8d237d589864d365
Wayne Starr <Racer159@users.noreply.github.com>
2018-05-11 09:06:45 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TOKEN="<token>"
+export GROUP="<group_id>"
+export NAME="Tenisha"
+export URL="<server_url>"
+export AVATAR="<avatar_url>"
+export PORT="<port_number>"
+
+nohup nodejs app.js &
